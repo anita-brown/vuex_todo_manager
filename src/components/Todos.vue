b246ebda360aef94cb2b9de08186ee0d28162d4c
Anita <Efosa@Anita-Efosa.local>
2022-07-07 10:57:38 +0100

    components refactoring to add global styles

---
+++ src/components/Todos.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Todos</div>
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Todos",
+};
+</script>
+<style></style>
