5564c0ca60f7134048e15dc331af98704f3295d7
Anita <Efosa@Anita-Efosa.local>
2022-07-07 11:03:38 +0100

    vuex store module created to manage todos

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="container">
-    <Todos/>
+    <Todos />
   </div>
 </template>
 
 <script>
-import Todos from './components/Todos.vue'
+import Todos from "./components/Todos.vue";
 
 export default {
-  name: 'App', 
+  name: "App",
   components: {
-    Todos
-  }
-}
+    Todos,
+  },
+};
 </script>
 
 <style>
 body {
-  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
   line-height: 1.6;
   background: #e8f7f0;
 }
